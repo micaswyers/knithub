88cd474fc13c83983523d95e09c1dc7e1f235d13
Mica Swyers <micaswyers@gmail.com>
2016-11-05 20:05:37 -0700

    Update script to take in file, w, h inputs

--- convert.py
+++ convert.py
@@ -1,18 +1,29 @@
 from PIL import Image
+import csv
+import sys
 
-image = Image.open('chart.png')
-image = image.resize((17*5,14*5))
-width, height = image.size
-#print "%s x %s" % (width, height)
-# Load image data
-data = list(image.getdata())
-for y in xrange(14):
-    for x in xrange(17):
-        center_y = y * height/14 + height/14/2
-        center_x = x * width/17 + width/17/2
-        pos = center_y * width + center_x
-        val = data[pos][0] < 128 # If < half-black, count as black. ROBUST.
-        print ' X'[val] + ",",
-    print
+def convert_chart(img_file, width, height):
+    image = Image.open(img_file)
+    image = image.resize((17*5,14*5))
+    width, height = image.size
+    #print "%s x %s" % (width, height)
+    # Load image data
+    data = list(image.getdata())
+    with open('converted_chart.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for y in xrange(14):
+            row = []
+            for x in xrange(17):
+                center_y = y * height/14 + height/14/2
+                center_x = x * width/17 + width/17/2
+                pos = center_y * width + center_x
+                val = data[pos][0] < 128 # If < half-black, count as black
+                stitch = ' V'[val]
+                row.append(stitch)
+            writer.writerow(row)
 
-# TODO: Resize image slightly smaller to increase likelihood of hitting the "target"
+if __name__ == "__main__":
+    img = input("Image filename? (Put in quotes)  ")
+    width = input("Chart width (in sts)? ")
+    height = input("Chart height (in sts)? ")
+    convert_chart(img, width, height)
