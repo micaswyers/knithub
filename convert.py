ffe62dc13d8988368a70428c03d6d4ae6e547064
Mica Swyers <micaswyers@gmail.com>
2016-11-05 21:00:55 -0700

    Take in input width and height

--- convert.py
+++ convert.py
@@ -2,20 +2,19 @@ from PIL import Image
 import csv
 import sys
 
-def convert_chart(img_file, width, height):
+def convert_chart(img_file, input_width, input_height):
     image = Image.open(img_file)
-    image = image.resize((17*5,14*5))
+    image = image.resize((input_width*5, input_height*5))
     width, height = image.size
-    #print "%s x %s" % (width, height)
     # Load image data
     data = list(image.getdata())
     with open('converted_chart.csv', 'wb') as csvfile:
         writer = csv.writer(csvfile)
-        for y in xrange(14):
+        for y in xrange(input_height):
             row = []
-            for x in xrange(17):
-                center_y = y * height/14 + height/14/2
-                center_x = x * width/17 + width/17/2
+            for x in xrange(input_width):
+                center_y = y * height/input_height + height/input_height/2
+                center_x = x * width/input_width + width/input_width/2
                 pos = center_y * width + center_x
                 val = data[pos][0] < 128 # If < half-black, count as black
                 stitch = ' V'[val]
